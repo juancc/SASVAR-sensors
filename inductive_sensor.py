27402396a96b056df0e64446a1f473251a52ae35
JCA <juanarbelaez@vaico.com.co>
2022-11-02 12:32:09 -0500

    refactor and simplify code

--- inductive_sensor.py
+++ inductive_sensor.py
@@ -1,27 +1,13 @@
 import time
 import RPi.GPIO as GPIO
 
-def initialInductive(pin):
-    global GPIOpin 
-    GPIOpin = pin
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIOpin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    print("Finished Initiation")
-    print(GPIOpin)
+PIN = 17
 
-def detectMetal():
-    state = False
-    if(GPIOpin != -1):
-        state = GPIO.input(GPIOpin)
-    else:
-        print("Please Initial Input Pin")
-    return state
-
-# test module
 if __name__ == '__main__':
     pin = 17
-    initialInductive(pin)
+    GPIO.setmode(GPIO.BCM)
+
     while True:
-        is_metal = detectMetal()
-        print(is_metal)
+        state = GPIO.input(PIN)
+        print(state)
         time.sleep(0.2)
